d0334f7b811e959556ed55f5213d380e13f36172
sekrittt <61773395+sekrittt@users.noreply.github.com>
2022-10-29 17:06:06 +0500

    update

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 # for page in web('ООО "АГРОПЛАЗМА"').pages:
 #     print(page)
-import os, json
+import os, requests, re
+from bs4 import BeautifulSoup
 import pandas as pd
 os.system('clear')
 
@@ -14,7 +15,6 @@ excel_4 = pd.read_excel('4. Технологии.xlsx')
 help_branch = pd.read_excel('Справочник. Отрасли и подотрасли.xlsx')
 help_tech = pd.read_excel('Справочник. Технологии.xlsx')
 
-
 def format_help_tech(d: pd.DataFrame):
     branches = {}
     for index, row in d.iterrows():
@@ -22,28 +22,47 @@ def format_help_tech(d: pd.DataFrame):
             continue
 
         if row[1] in branches:
-            if row[3] in branches[row[1]]['sub']:
-                branches[row[1]]['sub'][row[3]]['sub'].append(row[5])
+            if row[3] in branches[row[1]]:
+                branches[row[1]][row[3]].append(row[5])
                 continue
-            branches[row[1]]['sub'][row[3]] = {
-                'sub': [
+            branches[row[1]][row[3]] = [
                     row[5]
                 ]
-            }
             continue
         branches[row[1]] = {
-            'sub': {
-                row[3]: {
-                    'sub': [
+                row[3]: [
                         row[5]
                     ]
                 }
-            }
-        }
+    return branches
+
+def format_help_branches(d: pd.DataFrame):
+    branches = {}
+    for index, row in d.iterrows():
+        if index == 0 or index == 1:
+            continue
+
+        if row[1] in branches:
+            branches[row[1]].append(row[3])
+            continue
+        branches[row[1]] = [
+                row[3]
+            ]
     return branches
         
+help_tech_formatted = format_help_tech(help_tech)
+help_branch_formatted = format_help_branches(help_branch)
 
-help_branch_formatted = format_help_tech(help_tech)
+def get_text_from_site(url: str):
+    site = requests.get(url)
+    site.encoding = site.apparent_encoding
+    html = BeautifulSoup(site.text, 'html.parser')
 
-#print(help_branch_formatted)
+    text = re.sub(r'\s+', ' ', html.get_text())
+    return text
 
+for index, row in excel_1.iterrows():
+    if index == 0:
+        continue
+    site = row[7]
+    if 
--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-# import requests
+import requests, re
+from bs4 import BeautifulSoup
 
-# site = requests.get('http://www.agroplazma.com')
-# # print(site.text)
+site = requests.get('http://www.agroplazma.com')
+site.encoding = site.apparent_encoding
+html = BeautifulSoup(site.text, 'html.parser')
+
+text = re.sub(r'\s+', ' ',html.get_text())
+print(text)
